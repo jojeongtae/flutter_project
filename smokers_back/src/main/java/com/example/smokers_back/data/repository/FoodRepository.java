d335422bf860d027d100081d75e338e748b47358
choi-seonah <dondo0412@naver.com>
2025-07-01 10:16:19 +0900

    ㅋㅋㅈㅅ

--- smokers_back/src/main/java/com/example/smokers_back/data/repository/FoodRepository.java
+++ smokers_back/src/main/java/com/example/smokers_back/data/repository/FoodRepository.java
@@ -3,5 +3,5 @@ package com.example.smokers_back.data.repository;
 import com.example.smokers_back.data.entity.FoodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class FoodRepository extends JpaRepository<FoodEntity, Integer> {
+public interface FoodRepository extends JpaRepository<FoodEntity, Integer> {
 }
